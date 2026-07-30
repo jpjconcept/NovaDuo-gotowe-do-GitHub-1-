b8d98bf35c8e42ea420c06ba9c4c6bff71ce3723
jpjconcept <jpjconcept@gmail.com>
2026-07-30 14:07:25 +0200

    Dodanie podstrony Segmenty Pogroszew do mapy strony

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,15 +1,31 @@
 export default function sitemap() {
   const baseUrl = "https://www.jpjconcept.pl";
+  const lastModified = new Date();
 
   return [
     {
       url: `${baseUrl}/`,
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 1,
     },
     {
       url: `${baseUrl}/segmenty-pod-warszawa`,
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/segmenty-pogroszew`,
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.9,
     },
     {
       url: `${baseUrl}/historia-cen`,
+      lastModified,
+      changeFrequency: "daily",
+      priority: 0.7,
     },
   ];
 }
