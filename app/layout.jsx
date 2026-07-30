b4b132dddd93c946a36e48c49a00e5eff22ee33f
jpjconcept <jpjconcept@gmail.com>
2026-07-30 16:00:18 +0200

    Poprawa domeny i ustawień zdjęcia dla Google

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,56 +1,123 @@
 import "./globals.css";
 
 export const metadata = {
-  title:
-    "NovaDuo – Domy i bliźniaki Pogroszew | Ożarów Mazowiecki | JPJ Concept",
+  metadataBase: new URL("https://www.jpjconcept.pl"),
+
+  title: {
+    default:
+      "NovaDuo – Segmenty, domy i bliźniaki w Pogroszewie | JPJ Concept",
+    template: "%s | NovaDuo",
+  },
 
   description:
-    "Nowoczesne domy dwulokalowe w Pogroszewie pod Ożarowem Mazowieckim. Kameralna inwestycja NovaDuo przy ul. Nowowiejskiej 58. Garaż, ogród, pompa ciepła i spokojna okolica.",
+    "Nowoczesne domy dwulokalowe w Pogroszewie, w gminie Ożarów Mazowiecki. Kameralna inwestycja NovaDuo przy ul. Nowowiejskiej 58. Garaż, prywatny ogród, pompa ciepła i rekuperacja.",
 
   keywords: [
-    "domy Pogroszew",
+    "segmenty Pogroszew",
+    "segmenty pod Warszawą",
     "bliźniaki Pogroszew",
+    "bliźniaki pod Warszawą",
+    "domy Pogroszew",
     "domy Ożarów Mazowiecki",
-    "bliźniaki Ożarów Mazowiecki",
     "domy pod Warszawą",
     "nowe domy Pogroszew",
-    "dom Stare Babice",
-    "dom okolice Kampinosu",
-    "bliźniak okolice Kampinosu",
-    "bliźniak okolice Stare Babice",
     "NovaDuo",
     "JPJ Concept",
     "dom z garażem",
     "dom z ogrodem",
+    "dom z pompą ciepła",
+    "dom z rekuperacją",
   ],
 
+  applicationName: "NovaDuo",
+  authors: [{ name: "JPJ Concept Sp. z o.o." }],
+  creator: "JPJ Concept Sp. z o.o.",
+  publisher: "JPJ Concept Sp. z o.o.",
+
+  alternates: {
+    canonical: "/",
+  },
+
   openGraph: {
+    title:
+      "NovaDuo – Segmenty, domy i bliźniaki w Pogroszewie pod Warszawą",
+    description:
+      "Kameralna inwestycja NovaDuo przy ul. Nowowiejskiej 58 w Pogroszewie. 8 lokali z garażami, prywatnymi ogrodami, pompami ciepła i rekuperacją.",
+    url: "/",
+    siteName: "NovaDuo",
+    locale: "pl_PL",
+    type: "website",
+    images: [
+      {
+        url: "/images/postprodukcja_0000_Scene-1_upscale01.png",
+        alt: "NovaDuo – nowe segmenty i bliźniaki w Pogroszewie pod Warszawą",
+      },
+    ],
+  },
 
-    title: "NovaDuo – Domy dwulokalowe w Pogroszewie",
- description:
-"Nowoczesne domy i bliźniaki w Pogroszewie pod Warszawą. Kameralna inwestycja NovaDuo przy ul. Nowowiejskiej 58. 8 lokali z garażem, działkami od 255 do 424 m² oraz pompą ciepła.",
-  url: "https://novaduo.pl",
-  siteName: "NovaDuo",
-  locale: "pl_PL",
-  type: "website",
-},
+  twitter: {
+    card: "summary_large_image",
+    title:
+      "NovaDuo – Segmenty, domy i bliźniaki w Pogroszewie pod Warszawą",
+    description:
+      "Kameralna inwestycja NovaDuo w Pogroszewie. Garaż, prywatny ogród, pompa ciepła i rekuperacja.",
+    images: ["/images/postprodukcja_0000_Scene-1_upscale01.png"],
+  },
 
-alternates: {
-  canonical: "https://novaduo.pl",
-},
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 
   icons: {
-  icon: "/icon.png",
-},
+    icon: "/icon.png",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
+  },
 };
+
 export default function RootLayout({ children }) {
+  const websiteStructuredData = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    name: "NovaDuo",
+    alternateName: "NovaDuo | JPJ Concept",
+    url: "https://www.jpjconcept.pl",
+    inLanguage: "pl-PL",
+    publisher: {
+      "@type": "Organization",
+      name: "JPJ Concept Sp. z o.o.",
+      url: "https://www.jpjconcept.pl",
+      logo: {
+        "@type": "ImageObject",
+        url: "https://www.jpjconcept.pl/images/logo-novaduo.png",
+      },
+    },
+    image:
+      "https://www.jpjconcept.pl/images/postprodukcja_0000_Scene-1_upscale01.png",
+  };
+
   return (
     <html lang="pl">
-      <head>
-        <link rel="icon" href="/icon.png" />
-      </head>
+      <body>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(websiteStructuredData),
+          }}
+        />
 
-      <body>{children}</body>
+        {children}
+      </body>
     </html>
   );
 }
