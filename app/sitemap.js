c77cc3a33dc9544918938474e1a0e0bb51d217b6
jpjconcept <jpjconcept@gmail.com>
2026-07-30 16:30:15 +0200

    Dodanie podstrony Domy z ogrodem do mapy strony

--- app/sitemap.js
+++ app/sitemap.js
@@ -40,6 +40,12 @@ export default function sitemap() {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/domy-z-ogrodem-pod-warszawa`,
+      lastModified,
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/historia-cen`,
       lastModified,
       changeFrequency: "daily",
