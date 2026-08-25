322a36aa9f0f241f2febaecc13ce285564768ab5
jpjconcept <jpjconcept@gmail.com>
2026-08-25 17:01:58 +0200

    Zmiana obrazu usytuowania na wersję z rezerwacjami

--- app/page.jsx
+++ app/page.jsx
@@ -949,7 +949,7 @@ export default function Page() {
 
         <div className="mb-12 overflow-hidden rounded-[2rem] bg-white shadow-2xl">
           <img
-            src="/images/usytuowanie-lokali-novaduo.webp"
+            src="/images/novaduo_rezerwacje.webp"
             alt="Usytuowanie lokali NovaDuo"
             className="mx-auto max-h-[500px] w-auto object-contain"
           />
